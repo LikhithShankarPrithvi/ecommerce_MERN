7b0c3dd2588c501aef23b7e3db153eac36d741d9
LikhithShankarPrithvi <likhithshankarprithvi@gmail.com>
2021-01-09 21:24:22 +0530

    react secondary setup

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap'
+
+const Header = () => {
+    return ( 
+        <Container>
+            <header>Header</header>
+        </Container>
+     );
+}
+ 
+export default Header;
