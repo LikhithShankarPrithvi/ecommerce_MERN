8e4183dc9ca691337418b036fcf10ad426bd6cb6
LikhithShankarPrithvi <likhithshankarprithvi@gmail.com>
2021-01-11 18:16:25 +0530

    Homescreen Building

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,18 @@ import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
 	return (
 		<>
 			<Header />
 			<main>
-				<Container className='p-3 mb-2 bg-dark text-white text-center'>
-					<h1>Welcome</h1>
+				<Container>
+					<HomeScreen />
 				</Container>
 				<Container className='p-3 mb-2 bg-warning text-dark'>
 					<h1>Hello my name is Shankar</h1>
-					<Row className='p-3 mb-2 bg-dark text-white'>
-						<h1>Hello my name is Shankar</h1>
-					</Row>
 				</Container>
 			</main>
 			<Footer />
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Product from "../components/Product";
+import { Col, Container, Row } from "react-bootstrap";
+import products from "../products";
+
+const HomeScreen = () => {
+	return (
+		<>
+			<Container>
+				<h1>Latest Products</h1>
+				<Row>
+					{products.map((product) => (
+						<Col sm={12} md={6} lg={4} xl={3}>
+							<Product product={product} />
+							{/* <h3>{product.name}</h3> */}
+						</Col>
+					))}
+				</Row>
+			</Container>
+		</>
+	);
+};
+
+export default HomeScreen;
