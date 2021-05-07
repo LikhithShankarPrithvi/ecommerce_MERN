c816553fa560c2da0e48ccc67c7f82b722591220
LikhithShankarPrithvi <likhithshankarprithvi@gmail.com>
2021-05-07 12:52:48 +0530

    playing with header UI

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { LinkContainer } from 'react-router-bootstrap';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
 const Header = () => {
 	return (
@@ -24,7 +24,7 @@ const Header = () => {
 				</Navbar>
 			</Container>
 		</header>
-	);
-};
+	)
+}
 
-export default Header;
+export default Header
