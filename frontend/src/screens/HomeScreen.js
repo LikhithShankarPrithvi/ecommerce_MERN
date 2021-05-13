9c9e21e304be22180f5039413255d87e303c012e
LikhithShankarPrithvi <likhithshankarprithvi@gmail.com>
2021-05-13 23:38:31 +0530

    redux init

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
 import { Col, Container, Row } from 'react-bootstrap'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
-	const [products, setProducts] = useState([])
+	const dispatch = useDispatch()
+
+	// const productList = useSelector((state) => state.productList)
+	// const { loading, error, products } = productList
 
 	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get('/api/products')
-
-			setProducts(data)
-		}
-
-		fetchProducts()
-	}, [])
+		dispatch(listProducts)
+	}, [dispatch])
+	const products = []
 	return (
 		<>
 			<Container>
 				<h1 className='my-3'> Latest Products</h1>
+				{/* {loading ? (
+					<h2>Loading...</h2>
+				) : error ? (
+					<h3>{error}</h3>
+				) : ( */}
 				<Row>
 					{products.map((product) => (
 						<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -27,6 +31,7 @@ const HomeScreen = () => {
 						</Col>
 					))}
 				</Row>
+				{/* )} */}
 			</Container>
 		</>
 	)
